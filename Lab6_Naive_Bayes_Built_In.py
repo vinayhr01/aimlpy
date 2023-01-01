d671e8a327e50aa35b2b712f325252f34999a774
VINAY H R <vinayhr1211@gmail.com>
2023-01-01 16:19:23 +0530

    Small Changes

--- Lab6_Naive_Bayes_Built_In.py
+++ Lab6_Naive_Bayes_Built_In.py
@@ -7,11 +7,11 @@ splitratio = 0.9
 
 trainSize = int(len(dataset) * splitratio)
 
-X_train = [[int(j) for j in i[:-1]] for i in dataset[:trainSize]]
-X_test = [[int(j) for j in i[:-1]] for i in dataset[trainSize:]]
+X_train = [[float(j) for j in i[:-1]] for i in dataset[:trainSize]]
+X_test = [[float(j) for j in i[:-1]] for i in dataset[trainSize:]]
 
-y_train = [int(i[-1]) for i in dataset[:trainSize]]
-y_test = [int(i[-1]) for i in dataset[trainSize:]]
+y_train = [float(i[-1]) for i in dataset[:trainSize]]
+y_test = [float(i[-1]) for i in dataset[trainSize:]]
 
 model = GaussianNB()
 model.fit(X_train, y_train)
