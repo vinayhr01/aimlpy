b27e686af11978c70cb058cd1963d4f82be2bfa5
VINAY H R <vinayhr1211@gmail.com>
2023-01-01 16:16:16 +0530

    Created a naive bayes built in program

---
+++ Lab6_Naive_Bayes_Built_In.py
@@ -0,0 +1,19 @@
+from sklearn.naive_bayes import GaussianNB
+import csv
+import numpy as np
+
+dataset = list(csv.reader(open('tsvnaive.csv')))
+splitratio = 0.9
+
+trainSize = int(len(dataset) * splitratio)
+
+X_train = [[int(j) for j in i[:-1]] for i in dataset[:trainSize]]
+X_test = [[int(j) for j in i[:-1]] for i in dataset[trainSize:]]
+
+y_train = [int(i[-1]) for i in dataset[:trainSize]]
+y_test = [int(i[-1]) for i in dataset[trainSize:]]
+
+
+model = GaussianNB()
+model.fit(X_train, y_train)
+print("Accuracy ", model.score(X_test,y_test)*100)
