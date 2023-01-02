3733f208e40e5c04a4ff966793e4bcbb84641cf4
VINAY H R <vinayhr1211@gmail.com>
2023-01-02 12:59:24 +0530

    small changes

--- Lab6_Naive_Bayes_Built_In.py
+++ Lab6_Naive_Bayes_Built_In.py
@@ -9,10 +9,11 @@ trainSize = int(len(dataset) * splitratio)
 
 print("Split {0} rows into train={1} and test={2} rows using built in functions".format(len(dataset),trainSize,len(dataset)-trainSize))
 
-X_train = [[float(j) for j in i[:-1]] for i in dataset[:trainSize]]
+X_train = [[float(j) for j in i[:-1]] for i in dataset[1:trainSize]]
+print(X_train)
 X_test = [[float(j) for j in i[:-1]] for i in dataset[trainSize:]]
 
-y_train = [float(i[-1]) for i in dataset[:trainSize]]
+y_train = [float(i[-1]) for i in dataset[1:trainSize]]
 y_test = [float(i[-1]) for i in dataset[trainSize:]]
 
 model = GaussianNB()
@@ -24,3 +25,49 @@ for i in range(len(X_test)):
        cnt += 1
 print("Accuracy ", (cnt/len(X_test))*100)
 print("Accuracy ", model.score(X_test,y_test)*100)
+'''
+import pandas as pd
+from sklearn import tree
+from sklearn.preprocessing import LabelEncoder
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+import sklearn.metrics as sm
+
+data=pd.read_csv('traintennis.csv')
+print("first five records\n",data.head())
+
+x=data.iloc[:,:-1]
+print("first five train data\n",x.head())
+
+y=data.iloc[:,-1]
+print("first five train output\n",y.head())
+
+x=x.copy()
+
+le_outlook = LabelEncoder()
+x.Outlook = le_outlook.fit_transform(x.Outlook)
+
+le_Temperature=LabelEncoder()
+x.Temperature=le_Outlook.fit_transform(x.Temperature)
+
+le_Humidity=LabelEncoder()
+x.Humidity=le_Outlook.fit_transform(x.Humidity)
+
+le_Wind=LabelEncoder()
+x.Wind=le_Outlook.fit_transform(x.Wind)
+
+print("after encoding train data\n",x.head())
+
+y=y.copy()
+
+le_PlayTennis=LabelEncoder()
+y=le_PlayTennis.fit_transform(y)
+
+print("after encoding test data\n",y)
+
+x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2)
+
+classifier=GaussianNB()
+classifier.fit(x_train,y_train)
+
+print("Accuracy is:", sm.accuracy_score(classifier.predict(X_test), y_test))'''
