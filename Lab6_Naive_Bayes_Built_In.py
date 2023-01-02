015cc2029733763a2fcad2902cc0769bc00b2353
VINAY H R <vinayhr1211@gmail.com>
2023-01-02 20:04:03 +0530

    small changes

--- Lab6_Naive_Bayes_Built_In.py
+++ Lab6_Naive_Bayes_Built_In.py
@@ -69,4 +69,4 @@ x_train, x_test, y_train, y_test=train_test_split(x,y,test_size=0.2,random_state
 classifier=GaussianNB()
 classifier.fit(x_train,y_train)
 
-print("Accuracy is:", sm.accuracy_score(classifier.predict(x_test), y_test)*100)
+print("Accuracy ", classifier.score(x_test, y_test)*100)
